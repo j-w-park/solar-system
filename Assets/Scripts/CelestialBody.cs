605b0c735915cc9306c9a7d3ace9da3c60f47edb
j-w-park <okml133@gmail.com>
2025-10-01 17:38:45 +0900

    Uses radius property for calculations
    Changes the script to use a property for the radius, rather than accessing the transform directly.
    
    This allows better control over the value, using a serialized field and OnValidate/Reset methods to keep the visual size consistent.
    Also fixes inconsistent naming.

--- Assets/Scripts/CelestialBody.cs
+++ Assets/Scripts/CelestialBody.cs
@@ -6,14 +6,31 @@ public class CelestialBody : MonoBehaviour
     public Vector3 initialVelocity;
     public Vector3 velocity;
 
+    [Min(0f)]
+    [SerializeField]
+    private float radius = 10f;
+    public float Radius => this.radius;
+    public float Mass => this.surfaceG * this.radius * this.radius;
+
     private Rigidbody rb;
-    public float radius => this.transform.localScale.x * 0.5f;
-    public float mass => this.surfaceG * this.radius * this.radius;
 
     private void Awake()
     {
         this.velocity = this.initialVelocity;
         this.rb = this.GetComponent<Rigidbody>();
+    }
+
+    private void OnValidate()
+    {
+        float d = Mathf.Max(0f, 2f * this.radius);
+        this.transform.localScale = new Vector3(d, d, d);
+    }
+
+    private void Reset()
+    {
+        this.radius = Mathf.Max(0f, this.transform.localScale.x * 0.5f);
+        float d = 2f * this.radius;
+        this.transform.localScale = new Vector3(d, d, d);
     }
 
     public void UpdateVelocity(ref CelestialBody[] others)
@@ -23,7 +40,7 @@ public class CelestialBody : MonoBehaviour
             if (o != this)
             {
                 Vector3 disp = o.rb.position - this.rb.position;
-                Vector3 acc = disp.normalized * o.mass / disp.sqrMagnitude;
+                Vector3 acc = disp.normalized * o.Mass / disp.sqrMagnitude;
                 this.velocity += acc * Time.fixedDeltaTime;
             }
         }
