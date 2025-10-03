da103430b3c7dd4f4b7bbe38572729de98926470
j-w-park <okml133@gmail.com>
2025-10-03 12:49:12 +0900

    refactor: Integrate NBodyLeapfrog class for orbit simulation

--- Assets/Scripts/OrbitDrawer.cs
+++ Assets/Scripts/OrbitDrawer.cs
@@ -12,23 +12,20 @@ public struct VirtualBody
 
 public class OrbitDrawer : MonoBehaviour
 {
-    [Min(0)] public int numSteps = 100;
+    [Min(0)] public int numSteps = 10_000;
+    [Range(0.02f, 1f)]
+    public float deltaTime = 0.02f;
     public CelestialBody relativeTo = null; // if set, draw orbits relative to this body
 
-    private int relativeIndex = -1;
     private CelestialBody[] bodies;
+
+    private NBodyLeapfrog leapfrogSolver;
+
+    private int relativeIndex = -1;
     private Vector3[][] trails;
 
-    // snapshots
-    private Vector3[] pos;
-    private Vector3[] vel;
-    private Vector3[] acc;
-    private float[] mass;
-    private float[] radius;
-
-    private bool hasCollision;
-    private int colA = -1, colB = -1, colStep = -1;
-    private int stepsUsed = 0;
+    private int colA = -1, colB = -1;
+    private int step = 0;
 
     private void Awake()
     {
@@ -51,16 +48,25 @@ public class OrbitDrawer : MonoBehaviour
 
     private void Init()
     {
-        this.bodies = FindObjectsByType<CelestialBody>(FindObjectsSortMode.None);
+        this.bodies = FindObjectsByType<CelestialBody>(FindObjectsSortMode.InstanceID);
         if (this.bodies == null || this.bodies.Length == 0)
         {
             return;
         }
 
+        this.leapfrogSolver = new NBodyLeapfrog(this.bodies, this.deltaTime);
+
+        int n = this.bodies.Length;
+
+        if (this.trails == null || this.trails.Length != n)
+        {
+            this.trails = new Vector3[n][];
+        }
+
         this.relativeIndex = -1;
         if (this.relativeTo != null)
         {
-            for (int i = 0; i < this.bodies.Length; ++i)
+            for (int i = 0; i < n; ++i)
             {
                 if (this.bodies[i] == this.relativeTo)
                 {
@@ -69,55 +75,6 @@ public class OrbitDrawer : MonoBehaviour
                 }
             }
         }
-
-        var n = this.bodies.Length;
-
-        this.pos = new Vector3[n];
-        this.vel = new Vector3[n];
-        this.acc = new Vector3[n];
-        this.mass = new float[n];
-        this.radius = new float[n];
-
-        for (int i = 0; i < n; ++i)
-        {
-            this.pos[i] = this.bodies[i].transform.position;
-            this.vel[i] = this.bodies[i].initialVelocity;
-            this.mass[i] = this.bodies[i].Mass;
-            this.radius[i] = this.bodies[i].Radius;
-        }
-
-        if (this.trails == null || this.trails.Length != n)
-        {
-            this.trails = new Vector3[n][];
-        }
-
-        // reset collision info
-        this.hasCollision = false;
-        this.colA = this.colB = -1;
-        this.colStep = -1;
-        this.stepsUsed = Math.Min(1, this.numSteps); // at least the initial point
-    }
-
-    private bool CheckCollision(out int a, out int b)
-    {
-        a = b = -1;
-
-        int n = this.bodies.Length;
-        for (int i = 0; i < n - 1; ++i)
-        {
-            for (int j = i + 1; j < n; ++j)
-            {
-                float dist = Vector3.Distance(this.pos[i], this.pos[j]);
-                if (dist < this.radius[i] + this.radius[j])
-                {
-                    a = i;
-                    b = j;
-                    return true;
-                }
-            }
-        }
-
-        return false;
     }
 
     private void Simulate()
@@ -134,48 +91,45 @@ public class OrbitDrawer : MonoBehaviour
             {
                 this.trails[i] = new Vector3[this.numSteps];
             }
-            this.trails[i][0] = this.pos[i];
+            this.trails[i][0] = this.leapfrogSolver.Pos[i];
         }
 
         // leapfrog: initial half-kick
-        NBodyLeapfrog.ComputeAccelerations(pos, mass, acc);
-        NBodyLeapfrog.InitialHalfKick(vel, acc, Time.fixedDeltaTime);
+        this.leapfrogSolver.ComputeAccelerations();
+        this.leapfrogSolver.InitialHalfKick();
 
         // main steps
-        for (int @is = 1; @is < this.numSteps; ++@is)
+        for (this.step = 1; this.step < this.numSteps; ++this.step)
         {
             // drift
-            NBodyLeapfrog.Drift(pos, vel, Time.fixedDeltaTime);
+            this.leapfrogSolver.Drift();
 
             // record position
             for (int i = 0; i < n; ++i)
             {
-                this.trails[i][@is] = this.pos[i];
+                this.trails[i][this.step] = this.leapfrogSolver.Pos[i];
             }
 
-            this.stepsUsed = @is + 1;
-
             // collision check
-            if (this.CheckCollision(out this.colA, out this.colB))
+            this.colA = this.colB = -1;
+            if (this.leapfrogSolver.CheckCollision(out this.colA, out this.colB))
             {
-                this.hasCollision = true;
-                this.colStep = @is;
                 break;
             }
 
-            NBodyLeapfrog.ComputeAccelerations(pos, mass, acc);
+            this.leapfrogSolver.ComputeAccelerations();
 
             // full kick except last
-            if (@is < numSteps - 1)
+            if (this.step < numSteps - 1)
             {
-                NBodyLeapfrog.FullKick(vel, acc, Time.fixedDeltaTime);
+                this.leapfrogSolver.FullKick();
             }
         }
 
-        if (!this.hasCollision)
+        if (this.colA <= 0 || this.colB <= 0)
         {
             // final half-kick to keep positions/velocities time-centered
-            NBodyLeapfrog.InitialHalfKick(vel, acc, Time.fixedDeltaTime);
+            this.leapfrogSolver.InitialHalfKick();
         }
     }
 
@@ -197,33 +151,33 @@ public class OrbitDrawer : MonoBehaviour
         for (int i = 0; i < n; ++i)
         {
             Gizmos.color = Color.HSVToRGB((float)i / Mathf.Max(1, n), 1f, 1f);
-            // draw only up to stepsUsed
-            for (int @is = 1; @is < this.stepsUsed; ++@is)
+            // draw only up to steps computed
+            for (int s = 1; s < this.step; ++s)
             {
-                Vector3 p0 = this.trails[i][@is - 1];
-                Vector3 p1 = this.trails[i][@is];
+                Vector3 p0 = this.trails[i][s - 1];
+                Vector3 p1 = this.trails[i][s];
                 if (this.relativeIndex >= 0)
                 {
-                    p0 = p0 - this.trails[this.relativeIndex][@is - 1] + anchor;
-                    p1 = p1 - this.trails[this.relativeIndex][@is] + anchor;
+                    p0 = p0 - this.trails[this.relativeIndex][s - 1] + anchor;
+                    p1 = p1 - this.trails[this.relativeIndex][s] + anchor;
                 }
                 Gizmos.DrawLine(p0, p1);
             }
         }
 
-        if (this.hasCollision && this.colA >= 0 && this.colB >= 0 && this.colStep >= 0)
+        if (this.colA >= 0 && this.colB >= 0)
         {
             Gizmos.color = Color.red;
-            Vector3 pa = this.trails[this.colA][this.colStep];
-            Vector3 pb = this.trails[this.colB][this.colStep];
+            Vector3 pa = this.trails[this.colA][this.step];
+            Vector3 pb = this.trails[this.colB][this.step];
             if (this.relativeIndex >= 0)
             {
-                var pref = this.trails[this.relativeIndex][this.colStep];
+                Vector3 pref = this.trails[this.relativeIndex][this.step];
                 pa = pa - pref + anchor;
                 pb = pb - pref + anchor;
             }
-            float rA = this.radius[this.colA];
-            float rB = this.radius[this.colB];
+            float rA = this.bodies[this.colA].Radius;
+            float rB = this.bodies[this.colB].Radius;
             Gizmos.DrawSphere(pa, rA);
             Gizmos.DrawSphere(pb, rB);
         }
