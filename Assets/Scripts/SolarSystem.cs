71a1ed98335025087418e43b2d0ad29322934ebe
j-w-park <okml133@gmail.com>
2025-10-03 12:49:12 +0900

    refactor: Integrate NBodyLeapfrog class for real-time simulation

--- Assets/Scripts/SolarSystem.cs
+++ Assets/Scripts/SolarSystem.cs
@@ -5,32 +5,16 @@ public class SolarSystem : MonoBehaviour
     private CelestialBody[] bodies;
     private bool initialized = false;
 
-    // snapshots
-    private Vector3[] pos;
-    private Vector3[] vel;
-    private Vector3[] acc;
-    private float[] mass;
+    private NBodyLeapfrog leapfrogSolver;
 
     private void Awake()
     {
-        this.bodies = FindObjectsByType<CelestialBody>(FindObjectsSortMode.None);
+        this.bodies = FindObjectsByType<CelestialBody>(FindObjectsSortMode.InstanceID);
         if (this.bodies == null || this.bodies.Length == 0)
         {
             return;
         }
-
-        int n = this.bodies.Length;
-        this.pos = new Vector3[n];
-        this.vel = new Vector3[n];
-        this.acc = new Vector3[n];
-        this.mass = new float[n];
-
-        for (int i = 0; i < n; ++i)
-        {
-            this.pos[i] = this.bodies[i].transform.position;
-            this.vel[i] = this.bodies[i].initialVelocity;
-            this.mass[i] = this.bodies[i].Mass;
-        }
+        this.leapfrogSolver = new NBodyLeapfrog(this.bodies);
     }
 
     private void FixedUpdate()
@@ -43,19 +27,22 @@ public class SolarSystem : MonoBehaviour
         // initial half-kick
         if (!this.initialized)
         {
-            NBodyLeapfrog.ComputeAccelerations(this.pos, this.mass, this.acc);
-            NBodyLeapfrog.InitialHalfKick(this.vel, this.acc, Time.fixedDeltaTime);
+            this.leapfrogSolver.ComputeAccelerations();
+            this.leapfrogSolver.InitialHalfKick();
             this.initialized = true;
         }
 
         // drift
-        NBodyLeapfrog.Drift(this.pos, this.vel, Time.fixedDeltaTime);
+        this.leapfrogSolver.Drift();
+
+        // update positions
         for (int i = 0; i < this.bodies.Length; ++i)
         {
-            this.bodies[i].transform.position = this.pos[i];
+            this.bodies[i].transform.position = this.leapfrogSolver.Pos[i];
         }
+        
         // full kick
-        NBodyLeapfrog.ComputeAccelerations(this.pos, this.mass, this.acc);
-        NBodyLeapfrog.FullKick(this.vel, this.acc, Time.fixedDeltaTime);
+        this.leapfrogSolver.ComputeAccelerations();
+        this.leapfrogSolver.FullKick();
     }
 }
